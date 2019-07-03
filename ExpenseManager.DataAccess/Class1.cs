492b6b69110ef097e9f4567541130a426280ba4c
dudelis <fukszon.kostya@gmail.com>
2019-07-03 21:31:47 +0200

    Init

---
+++ ExpenseManager.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExpenseManager.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExpenseManager/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExpenseManager
+{
+    public class Class1
+    {
+    }
+}
