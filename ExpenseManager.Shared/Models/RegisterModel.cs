deb34ce4942d82f0e75a7bc748f25bba45381967
dudelis <fukszon.kostya@gmail.com>
2019-09-07 22:02:45 +0200

    Add register page

--- ExpenseManager.Shared/Models/RegisterModel.cs
+++ ExpenseManager.Shared/Models/RegisterModel.cs
@@ -8,6 +8,10 @@ namespace ExpenseManager.Shared.Models
     public class RegisterModel
     {
         [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
         public string UserName { get; set; }
 
         [Required]
