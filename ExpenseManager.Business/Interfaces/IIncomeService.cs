e259a3a6ecd193c0c1e112a115f049432ab686e6
dudelis <fukszon.kostya@gmail.com>
2019-09-12 01:08:51 +0200

    #35 - Add Service and Manager to the business layer

---
+++ ExpenseManager.Business/Interfaces/IIncomeService.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseManager.DataAccess.Interfaces;
+using ExpenseManager.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseManager.Business.Interfaces
+{
+    public interface IIncomeService: IServiceBase<Income, IIncomeRepository>
+    {
+
+    }
+}
